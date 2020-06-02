fc9b72f0bdde28c2e8f329534c8cae5ced399104
Chris Yoon <chy.yoon@gmail.com>
2020-06-02 15:33:55 -0700

    create a website where animals have conversations Co-authored-by: Jason Khan <jasonderekkhan@gmail.com> Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>why hello there!</li>");
-    $('li').click(function() {
-      alert('hi');
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+      });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+      });
     });
-  });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
