bb57fd44f2dac326b553e5f1f15332a466e3c14e
Chris Yoon <chy.yoon@gmail.com>
2020-06-02 16:27:00 -0700

    add a photo of a dog
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>
    > Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   $("button#meow").click(function() {
-    $("ul#webpage").prepend("<li>Bowwow!</li>");
-    $("ul#user").children("li").first().click(function() {
-      $(this).remove();
-      });
-    $("ul#webpage").children("li").first().click(function() {
-      $(this).remove();
-      });
+    $("ul#webpage").prepend("<li>Bowwwowww!</li>");
+    $("ul#user").show();
+    //$(".dog-hidden").before(<li></li>);
+    //$(".dog-showing").toggle();
+    // $("ul#user").children("li").first().click(function() {
+    //   $(this).remove();
+    //   });
+    // $("ul#webpage").children("li").first().click(function() {
+    //   $(this).remove();
+    //   });
     });
 
   $("button#bark").click(function() {
